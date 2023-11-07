966d53446f739654cb0e7d65066f912530c9b1d9
jrrodrigo421 <rjr89000@gmail.com>
2023-11-06 21:30:28 -0300

    algumas funções muito utilizada quando usamos listas

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -64,4 +64,94 @@ void main(List<String> arguments) {
   List<dynamic> rodrigo = ['Rodrigo Lopes', 1.84, 30];
   print('imprimindo lista ${rodrigo[2]}');
 
+  //seguindo o artigo
+  // https://codeburst.io/top-10-array-utility-methods-you-should-know-dart-feb2648ee3a2
+
+  //forEach() executa uma função em cada elemento da lista
+  var fruits = ['maça', 'laranja', 'manga'];
+  fruits.forEach((fruit) => print(fruit));
+
+  //map() Produz uma nova lista depois de
+  // transformar cada elemento em uma determinada lista
+  var mappedFruits  = fruits.map((fruit) => 'Teste $fruit').toList();
+  print(mappedFruits);
+
+  //contains Verifica se o elemento fornecido está na lista
+  print(fruits.contains('laranja'));
+
+  // sort() Ordene os elementos com base na função de ordenação fornecida
+
+  List<int> numeros = [1,3,2,5,4];
+  numeros.sort((num1, num2) => (num1 - num2));
+  print(numeros);
+
+  // reduce() &  fold() Comprime os elementos para um único valor,
+  // usando a função fornecida
+
+  var sum = numeros.reduce((value, element) => value + element);
+  print(sum);
+
+  var initialValue = 10 ;
+  var sum2 = numeros.fold(initialValue, (previousValue, element) => previousValue + element);
+  print(sum2);
+
+  // every() Confirma que todos os elementos satisfazem o teste
+
+  List<Map<String, dynamic>> users  = [
+    {"name": 'RodrigoJr', "age": 30},
+    {"name": 'Rodrigo', "age": 50},
+    {"name": 'RAdriana', "age": 50},
+  ];
+
+  var is18AndOver = users.every((element) => element["age"]>=18);
+  print(is18AndOver);
+  var hasNamesWithR = users.every((element) => element["name"].startsWith('R'));
+  print(hasNamesWithR);
+
+  // where() , firstWhere(), singleWhere()
+  // Retorna uma coleção de elementos que satisfazem um teste.
+
+  var over21s = users.where((element) => element["age"] > 21);
+  print(over21s.length);
+
+  var nameR = users.firstWhere((element) => element["name"].startsWith('R'), orElse: () =>
+  {"name": '', "age": 0});
+  print(nameR);
+
+  List<Map<String, dynamic>> users2  = [
+    {"name": 'RodrigoJr', "age": 30},
+    {"name": 'Rodrigo', "age": 50},
+    {"name": 'RAdriana', "age": 50},
+  ];
+
+  var under18s = users2.singleWhere((element) => element["age"] < 18, orElse: () => {"name": '', "age": 0});
+  print(under18s);
+
+
+  // take() skip()
+  // Retorna uma coleção ao incluir ou ignorar elementos
+
+  var fiboNumbers =[1, 2, 3, 5,8 ,13, 21];
+  print(fiboNumbers.take(3).toList());
+  print(fiboNumbers.skip(5).toList());
+  print(fiboNumbers.take(3).skip(2).take(1));
+
+
+  // List.from()
+  // Cria uma nova lista a partir da coleção fornecida
+
+  var clonedFiboNumbers = List.from(fiboNumbers);
+  print('lista clonada: $clonedFiboNumbers');
+
+  var pars = [[1,2], [3,4]];
+  var flattened = pars.expand((element) => element.toList());
+  print('Flattened result: $flattened');
+
+  var input = [1, 2, 3];
+  var duplicated = input.expand((element) => [element, element].toList());
+  print(duplicated);
+
+
 }
+
+
