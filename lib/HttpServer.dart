5d1e4344e17c4055db36f32bbb484f4260b92bbc
jrrodrigo421 <rjr89000@gmail.com>
2023-11-07 11:27:34 -0300

    mais testes HttpClient

--- lib/HttpServer.dart
+++ lib/HttpServer.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main() async{
@@ -20,5 +21,18 @@ void main() async{
       response.statusCode = HttpStatus.notFound;
       }
       response.close();
+
+
+      // void main() async {
+      //   var url = Uri.parse('http://localhost:8888/dart');
+      //   var httpClient = HttpClient();
+      //   var request = await httpClient.getUrl(url);
+      //   var response = await request.close();
+      //   var data = await utf8.decoder.bind(response).toList();
+      //   print('Response ${response.statusCode}: $data');
+      //   httpClient.close();
+      // }
+
+
     }
 
