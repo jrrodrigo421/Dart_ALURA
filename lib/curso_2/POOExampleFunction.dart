94612248f0b86328c4fa98b0307c811aa3e273fd
jrrodrigo421 <rjr89000@gmail.com>
2023-11-07 22:31:08 -0300

    construtores e metodos da classe

--- lib/curso_2/POOExampleFunction.dart
+++ lib/curso_2/POOExampleFunction.dart
@@ -1,17 +1,37 @@
 main(){
  String toString =  retornaStrinfFormata(nome: 'laranja', peso: 25, diasColheita: 10);
   print(toString);
+
+  Fruta frutas01 = Fruta("laranja", 10.50, 'amarelo', 'citrico', 20, 15);
+  print(frutas01.nome);
+
+  Fruta fruta02 = Fruta.nomeados(nome: 'maca', cor: 'red', peso: 10.0, sabor: 'doce', diasColheita: 25, diasAteFicarMadura: 10);
+  print(fruta02.nome);
+
+  fruta02 = Fruta.minusculas('LARANJA');
+  print(fruta02.nome);
 }
 
 class Fruta{
 
-  late String nome;
-  late double peso;
-  late int diasColheita;
-  late int diasAteFicarMadura;
-  late bool isMadura;
+   late String nome;
+   late double peso;
+   late String cor;
+   late String sabor;
+   late int diasColheita;
+   late int diasAteFicarMadura;
+   late bool? isMadura;
 
-  Fruta(this.nome, this.peso, this.diasColheita, this.diasAteFicarMadura, this.isMadura);
+
+  //construtor com parametro posicional obrigatorio
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasColheita, this.diasAteFicarMadura, {this.isMadura});
+//podemos passar contrutores personalizados nessa caso utilizamos exemplo de parametros nomeados requiridos
+  Fruta.nomeados({required this.nome, required this.cor, required this.peso, required this.sabor, required this.diasColheita, required this.diasAteFicarMadura});
+
+  Fruta.minusculas(this.nome){
+    nome = nome.toLowerCase();
+
+  }
 
 }
 
