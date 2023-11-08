f729175ba3292e12882ad3b0da4f56a0b94c1ecf
jrrodrigo421 <rjr89000@gmail.com>
2023-11-08 14:35:00 -0300

    enum exemplo uso default

--- lib/curso_3/Enum.dart
+++ lib/curso_3/Enum.dart
@@ -1,17 +1,73 @@
 void main(){
-  escolherMeioDeTransporteEnum(Transporte.trem);
-
+  // escolherMeioDeTransporteEnum(Transporte.trem);
+  escolherMeioDeTransporteEnum(Transporte.aviao);
+  // escolherMeioDeTransporteEnum(Transporte.carro);
+  // escolherMeioDeTransporteEnum(Transporte.skate);
+  // escolherMeioDeTransporteEnum(Transporte.patins);
 }
 
-void escolherMeioDeTransporteEnum(Transporte locomocao){
-    if(locomocao == Transporte.carro){
-      print('Vou de carro');
-    }else if(locomocao == Transporte.bike){
-      print('Bike');
-    }else{
-      print('Outro transporte');
-    }
+void escolherMeioDeTransporteEnum(Transporte transporte) {
+  // if(transporte == Transporte.carro){
+  //   print('Vou de carro');
+  // }else if(transporte == Transporte.bike){
+  //   print('Bike');
+  // }else{
+  //   print('Outro transporte');
+  // }
+  print('Entrou aqui');
+
+  //switch com enum incompleto obrigatorio colocar o default e passar algo caso o parametro nao for encontrado..
+  // switch (transporte) {
+  //   case Transporte.carro:
+  //     print('É carro');
+  //     break;
+  //   case Transporte.bike:
+  //     print('É bike');
+  //     break;
+  //   case Transporte.andando:
+  //     print('É andando');
+  //     break;
+  //   case Transporte.skate:
+  //     print('É skate');
+  //     break;
+  //   case Transporte.aviao:
+  //     print('É skate');
+  //     break;
+  //   case Transporte.trem:
+  //     print('É trem');
+  //
+  //   default:
+  // print('testando default');
+  //
+  // }
+
+  //switch com enum completo
+  switch (transporte) {
+    case Transporte.carro:
+      print('É carro');
+      break;
+    case Transporte.bike:
+      print('É bike');
+      break;
+    case Transporte.andando:
+      print('É andando');
+      break;
+    case Transporte.skate:
+      print('É skate');
+    case Transporte.patins:
+      print('Patins');
+      break;
+    // case Transporte.aviao:
+    //   print('É skate');
+    //   break;
+    case Transporte.trem:
+      print('É trem');
+    default:
+      print('testando default');
+  }
 }
+
+
 
 enum Transporte{
   carro,
