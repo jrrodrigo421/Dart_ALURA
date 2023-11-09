c7e5db77cc2bd93a8c371aae4683ec831a4814f2
jrrodrigo421 <rjr89000@gmail.com>
2023-11-09 15:20:48 -0300

    pequena refatoracao

---
+++ lib/curso_4/helpers/helperStack.dart
@@ -0,0 +1,67 @@
+
+// Tema : tratamento de exceção testando  coneceito de pilha de execução :
+
+import 'dart:io';
+
+helperSatck(){
+  print('Started Main');
+  functionOne();
+  print('finish Main');
+}
+
+void functionOne() {
+  print('Started FunctionONe');
+
+  //try on usa quando sabemos qual o erro que vai ser gerado
+
+
+  try{
+    functionTwo();
+
+  }on IOException catch(exception, stackTrace) {
+    print(stackTrace.toString());
+    print("capiturado pelo functionOne");
+    // print(exception.message);
+    // print(exception.offset);
+    // print(exception.source);
+    print(exception.runtimeType);
+    print(exception.toString());
+
+    //vai continuar o fluxo caindo na main e quebrando o codigo pois ela na esta preparada para a exception
+    rethrow;
+
+
+  }
+  // }on HttpException catch(e){
+  //   print(e.uri);
+  //
+  // }on IOException catch(e){
+  //   print('teste');
+  //   print(e.toString());
+  //
+  //   //quando nao sabemos quando a excecao usamos direto o Exception pra pegar a excecao
+  // } on Exception catch(e){
+  //   print('teste');
+  //   e.toString();
+  // }catch(e){
+  //   print('teste');
+  //   print(e.toString());
+  // }
+    finally{
+    print('chegou no finally');
+  }
+  print('finalized functionOne');
+}
+
+void functionTwo() {
+  print('Started functionTWO');
+
+  for(int i = 0; i <= 5 ; i++){
+    print('VEZ $i');
+
+
+    double amount = double.parse("Not a number");
+  }
+  print('finish functionTWO');
+}
+
