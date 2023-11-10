50c8f18fbd4fac9362b9bb62eaa2995dc7d64370
jrrodrigo421 <rjr89000@gmail.com>
2023-11-09 21:26:36 -0300

    testando null safety

--- lib/curso_4/Null.dart
+++ lib/curso_4/Null.dart
@@ -2,4 +2,16 @@ main(){
   print('teste'.runtimeType);
   print(null.runtimeType);
   Null;
+
+
+  // isEmpty(null);
+
+  // String teste = 'teste'
+  // teste = null;
+
+  String? teste2 = 'teste';
+  teste2 = null;
+
 }
+
+bool isEmpty(String string) => string.length == 0;
