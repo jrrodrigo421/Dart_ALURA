e0e54aa5db9b3855357f7e927d01616c3875b2fe
jrrodrigo421 <rjr89000@gmail.com>
2023-11-10 10:12:31 -0300

    assincronismo Future e Stream

--- lib/curso_5/assincronismo.dart
+++ lib/curso_5/assincronismo.dart
@@ -2,4 +2,23 @@
 void main() async {
   final result = await Future.delayed(Duration(seconds: 5));
   print(result);
+
+  Future<int> myFutureFunc() async{
+    await Future.delayed(Duration(seconds: 3));
+    print('testeeeee');
+    return 30;
+
+  }
+  print(myFutureFunc());
+
+  Stream<int> timedCounter(int interval, [int? maxCount]) async* {
+    int i = 1;
+    while(i != maxCount){
+      await Future.delayed(Duration(seconds: interval));
+      print('dentro do loop do sream');
+      yield i++;
+    }
+  }
+
+  print(timedCounter(3,1000));
 }
