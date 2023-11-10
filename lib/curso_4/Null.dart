f2a9b38e64a368e035d8e3481ef7b885b813ea0b
jrrodrigo421 <rjr89000@gmail.com>
2023-11-09 21:33:06 -0300

    testando null check !

--- lib/curso_4/Null.dart
+++ lib/curso_4/Null.dart
@@ -1,3 +1,5 @@
+import 'package:projeto_inicial/curso_4/models/account.dart';
+
 main(){
   print('teste'.runtimeType);
   print(null.runtimeType);
@@ -12,6 +14,10 @@ main(){
   String? teste2 = 'teste';
   teste2 = null;
 
+  Account? account; //= Account(name: "name", balance: 300, isAuthenticated: true);
+  account.runtimeType;
+  account.toString();
+  account!.name;
 }
 
 bool isEmpty(String string) => string.length == 0;
